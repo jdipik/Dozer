384d9008658c176f260f98f865485a8e9361af3f
Roberto Rossi <roberto.rossi@progettopa.it>
2021-10-26 22:12:03 +0200

    J: first commit

---
+++ src/main/java/rjr/studio/Dozer/DozerApplication.java
@@ -0,0 +1,13 @@
+package rjr.studio.Dozer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DozerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DozerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rjr/studio/Dozer/DozerApplicationTests.java
@@ -0,0 +1,13 @@
+package rjr.studio.Dozer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DozerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
